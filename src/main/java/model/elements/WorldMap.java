680c794d76ba08e799f5be8aedc06b43cebe44c7
witek niec <witold.niec@gmail.com>
2025-01-06 16:42:20 +0100

    Added AbstractWorldMap

---
+++ src/main/java/model/elements/WorldMap.java
@@ -0,0 +1,47 @@
+package model.elements;
+
+
+import model.MoveValidator;
+import model.elements.animal.Animal;
+
+import java.util.List;
+
+public interface WorldMap extends MoveValidator {
+    /**
+     * Place a animal on the map.
+     *
+     * @param animal The animal to place on the map.
+     * @return True if the animal was placed. The animal cannot be placed if the move is not valid.
+     */
+    boolean place(Animal animal);
+
+    /**
+     * Moves an animal (if it is present on the map) according to specified direction.
+     * If the move is not possible, this method has no effect.
+     */
+    void move(Animal animal);
+
+    /**
+     * Return true if given position on the map is occupied. Should not be
+     * confused with canMove since there might be empty positions where the animal
+     * cannot move.
+     *
+     * @param position Position to check.
+     * @return True if the position is occupied.
+     */
+    boolean isOccupied(Vector2D position);
+
+    /**
+     * Return an animal at a given position.
+     *
+     * @param position The position of the animal.
+     * @return animal or null if the position is not occupied.
+     */
+    WorldElement objectAt(Vector2D position);
+
+
+    List<WorldElement> getElements();
+
+    int getId();
+}
+
