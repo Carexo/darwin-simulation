fd0badba364d2a733b781164a39bc78a0c3c2c0a
Carexo <marcel.mietla3@gmail.com>
2025-01-10 09:07:04 +0100

    fix typos in Genome class

--- src/main/java/model/elements/animal/Genome.java
+++ src/main/java/model/elements/animal/Genome.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Genom {
+public class Genome {
     private final int length;
     private int activeGene;
     private final List<Gene> genes;
 
-    public Genom(int length) {
+    public Genome(int length) {
         this.length = length;
         this.activeGene = 0;
 
@@ -18,8 +18,8 @@ public class Genom {
     }
 
 
-    public static Genom RandomGenome(int length) {
-        Genom genome = new Genom(length);
+    public static Genome RandomGenome(int length) {
+        Genome genome = new Genome(length);
 
         for(int i = 0; i < length; i++){
             genome.genes.add(Gene.getRandom());
@@ -28,10 +28,10 @@ public class Genom {
         return genome;
     }
 
-    public Genom combineGenomes(Genom rightGenes, float percentOfGenes) {
+    public Genome combineGenomes(Genome rightGenes, float percentOfGenes) {
         int rightGenesCount = (int)Math.ceil(length * percentOfGenes);
 
-        Genom genome = new Genom(length);
+        Genome genome = new Genome(length);
         genome.genes.addAll(this.genes.subList(0, length - rightGenesCount));
         genome.genes.addAll(rightGenes.genes.subList(length - rightGenesCount, length));
 
@@ -78,8 +78,8 @@ public class Genom {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Genom genom = (Genom) o;
-        return length == genom.length && activeGene == genom.activeGene && Objects.equals(genes, genom.genes);
+        Genome genome = (Genome) o;
+        return length == genome.length && activeGene == genome.activeGene && Objects.equals(genes, genome.genes);
     }
 
     @Override
